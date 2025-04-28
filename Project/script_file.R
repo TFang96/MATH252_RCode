794a0dd5c6f7bd894a6663bf5aaa6ebedb711f27
Tony Fang <78616310+TFang96@users.noreply.github.com>
2025-04-28 10:54:03 -0700

    Add code for CNMixt.

--- Project/script_file.R
+++ Project/script_file.R
@@ -28,7 +28,6 @@ real_estate_scaled <- scale(real_estate_data) # scale the data
 real_estate_dbscan <- dbscan(real_estate_scaled, eps = 2.75, minPts = 8) #dbscan
 real_estate_optics <- optics(real_estate_scaled, minPts = 8) #optics
 real_estate_pam <- pam(real_estate_data, k = 3)
-real_estate_cn <- CNmixt(real_estate_data, G = 3, model = "VVV", initialization = "kmeans")
 
 ## cluster the magic data ##
 magic_data_scaled <- scale(magic_data)
@@ -37,4 +36,76 @@ magic_optics <- optics(magic_scaled, minPts = 8)
 magic_pam <- pam(magic_data, k = 2)
 
 
+## CN Mixture
 
+fit_best_CN_model <- function(data, G_range = 2:4) {
+  tryCatch({
+    best_model <- NULL
+    best_bic <- -Inf
+    
+    for (g in G_range) {
+      model_names <- c("EEE", "VVV", "EEV", "VEV")
+      
+      for (model_name in model_names) {
+        fit <- tryCatch({
+          fit_CNmixt(data, G = g, model = model_name)
+        }, error = function(e) NULL)
+        
+        if (!is.null(fit)) {
+          if (!is.null(fit$bic) && fit$bic > best_bic) {
+            best_bic <- fit$bic
+            best_model <- fit
+          }
+        }
+      }
+    }
+    
+    if (is.null(best_model)) {
+      warning("All CNmixt models failed. Falling back to kmeans.")
+      km <- kmeans(data, centers = mean(G_range), nstart = 10)
+      return(list(
+        cluster = km$cluster,
+        outliers = rep(FALSE, nrow(data)),
+        outlier_pct = 0,
+        modelName = "Fallback_KMeans",
+        BIC = NA,
+        parameters = NULL,
+        model = km
+      ))
+    }
+    
+    cluster <- best_model$classification
+    outliers <- best_model$outliers
+    outlier_pct <- sum(outliers) / length(outliers) * 100
+    
+    return(list(
+      cluster = cluster,
+      outliers = outliers,
+      outlier_pct = outlier_pct,
+      modelName = best_model$modelName,
+      BIC = best_model$bic,
+      G = best_model$G,  # Save best G
+      parameters = best_model$parameters,
+      model = best_model
+    ))
+    
+  }, error = function(e) {
+    warning(paste("CNmixt fitting failed:", e$message))
+    km <- kmeans(data, centers = mean(G_range), nstart = 10)
+    return(list(
+      cluster = km$cluster,
+      outliers = rep(FALSE, nrow(data)),
+      outlier_pct = 0,
+      modelName = "Fallback_KMeans",
+      BIC = NA,
+      parameters = NULL,
+      model = km
+    ))
+  })
+}
+
+#CN for real estate
+best_fit_cn_res_estate <- fit_best_CN_model(real_estate_data)
+
+#CN for magic
+best_fit_cn_res_magic <- fit_best_CN_model(magic_data_numeric)
