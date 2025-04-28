5ad5dc2f5c6912650010b9bdcbe31f8a24cc5ae7
Tony Fang <78616310+TFang96@users.noreply.github.com>
2025-04-28 10:51:19 -0700

    Add code for CNMixt.

--- Project/script_file.R
+++ Project/script_file.R
@@ -4,10 +4,10 @@ library(dbscan) ## this is needed to run dbscan
 library(cluster) ## this is needed to run pam
 library(ContaminatedMixt) # needed to run contaminated normal
 library(NbClust) # needed to check the number of clusters a dataset has
-real_estate_data <- read_excel("C:/Users/tf245/Documents/GitHub/MATH252/Project/real+estate+valuation+data+set/Real estate valuation data set.xlsx") # read in real estate data
+real_estate_data <- read_excel("C:/Users/tf245/Documents/GitHub/MATH252_RCode/Project/real+estate+valuation+data+set/Real estate valuation data set.xlsx") # read in real estate data
 
 # Read the .names file as lines of text
-lines_magic_names <- readLines("C:/Users/tf245/Documents/GitHub/MATH252/Project/magic+gamma+telescope/magic04.names")
+lines_magic_names <- readLines("C:/Users/tf245/Documents/GitHub/MATH252_RCode/Project/magic+gamma+telescope/magic04.names")
 
 # Extract lines that start with a number and a period
 name_lines_magic <- grep("^\\d+\\.", lines_magic_names, value = TRUE)
@@ -16,12 +16,12 @@ name_lines_magic <- grep("^\\d+\\.", lines_magic_names, value = TRUE)
 col_names_magic <- sub("^\\d+\\.\\s*([a-zA-Z0-9_]+):.*", "\\1", name_lines_magic)
 
 #read the data file
-magic_data <- read_csv("C:/Users/tf245/Documents/GitHub/MATH252/Project/magic+gamma+telescope/magic04.data", col_names = col_names_magic)
+magic_data <- read_csv("C:/Users/tf245/Documents/GitHub/MATH252_RCode/Project/magic+gamma+telescope/magic04.data", col_names = col_names_magic)
 magic_data_numeric <- magic_data[, 1:10]
 
 #check for the number of clusters
-NbClust(real_estate_data, distance = "euclidean", min.nc = 2, max.nc = 15, method = "kmeans")
-NbClust(magic_data_numeric, distance = "euclidean", min.nc = 2, max.nc = 15, method = "kmeans")
+#NbClust(real_estate_data, distance = "euclidean", min.nc = 2, max.nc = 15, method = "kmeans")
+#NbClust(magic_data_numeric, distance = "euclidean", min.nc = 2, max.nc = 15, method = "kmeans")
 
 ## cluster real estate data ##
 real_estate_scaled <- scale(real_estate_data) # scale the data
